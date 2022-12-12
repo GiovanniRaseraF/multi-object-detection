9967aa933499dee250ffa97630589009699ee0af
Giovanni Rasera -- fdsa <45256169+GiovanniRaseraF@users.noreply.github.com>
2022-12-12 08:34:56 +0100

    3predic: merge into main

---
+++ 3predict/main.py
@@ -0,0 +1,169 @@
+import os
+import dataloader 
+from dataloader import Dataset
+from dataloader import ValDataset
+from dataloader import process_dataset as dataset
+from network import *
+import sys
+from predictor import *
+
+# Settable parameters
+#Get-Content input.txt |  C:/Users/fdsa/anaconda3/python.exe main.py 112 18000 1 2001 0 2001 0.000001 . ./models2
+
+scale           = 112 
+samples         = 9000
+samples         = 9000
+active_train    = False
+num_of_epochs   = 2001
+train_image_dir = '/assignment_1/train/images'
+train_ann_dir = '/assignment_1/train/annotations'
+val_image_dir   = '/assignment_1/test/images'
+load_model      = not active_train 
+number_model    = 2001
+lr              = 0.000001
+
+# specigic
+classes = [
+    'long sleeve dress',
+    'long sleeve outwear',
+    'long sleeve top', 
+    'short sleeve dress', 
+    'short sleeve outwear', 
+    'short sleeve top', 
+    'short', 
+    'skirt', 
+    'sling',
+    'sling dress',
+    'trousers', 
+    'vest', 
+    'vest dress'
+]
+
+changes = {
+	"long sleeve dress"     : 0,
+    "long sleeve outwear"   : 1,
+    "long sleeve top"       : 2,
+    "short sleeve dress"    : 3,
+    "short sleeve outwear"  : 4,
+    "short sleeve top"      : 5,
+    "shorts"                : 6,
+    "skirt"                 : 7,
+    "sling"                 : 8,
+    "sling dress"           : 9,
+    "trousers"              : 10,
+    "vest"                  : 11,
+    "vest dress"            : 12
+}
+
+def printdict(dictionary):
+    for key in (dictionary.items()):
+        print(f"{key}")
+
+if __name__ == "__main__":
+    print("usage: main.py scale samples samples2 active_train num_of_epochs load_model number_model lr from")
+    infodict = {
+        "scale"           : int(sys.argv[1]),
+        "samples"         : int(sys.argv[2]),
+        "samples2"        : int(sys.argv[3]),
+        "samples3"        : int(sys.argv[4]),
+        "active_train"    : bool(int(sys.argv[5])),
+        "num_of_epochs"   : int(sys.argv[6]),
+        "load_model"      : bool(int(sys.argv[7])),
+        "number_model"    : int(sys.argv[8]),
+        "lr"              : float(sys.argv[9]),
+        "fromdir"         : sys.argv[10].strip(),
+        "savedir"         : sys.argv[11].strip(),
+    }
+    scale           = infodict["scale"] 
+    samples         = infodict["samples"]
+    samples2        = infodict["samples2"]
+    samples3        = infodict["samples3"]
+    active_train    = infodict["active_train"]
+    num_of_epochs   = infodict["num_of_epochs"]
+    load_model      = infodict["load_model"] 
+    number_model    = infodict["number_model"] 
+    lr              = infodict["lr"] 
+    fromdir         = infodict["fromdir"]
+    savedir         = infodict["savedir"]
+    printdict(dictionary=infodict)
+
+    #Get-Content input.txt | C:/Users/fdsa/anaconda3/python.exe main.py 112 9359 9000 0 2001 1 461 0.00001 . ./models
+    
+    # Loading training dataset
+    train_images, train_labels, train_boxes = dataset(
+        image_dir   = fromdir + train_image_dir,
+        ann_dir     = fromdir + train_ann_dir +'/',
+        samples     = samples,
+        scale       = scale,
+        active_train= active_train,
+        changes     = changes
+    ) 
+
+    train_images2, train_labels2, train_boxes2 = dataset(
+        image_dir   = fromdir + train_image_dir + '2',
+        ann_dir     = fromdir + train_ann_dir + '2/',
+        samples     = samples2,
+        scale       = scale,
+        active_train= active_train,
+        changes     = changes
+    )
+
+    train_images3, train_labels3, train_boxes3 = dataset(
+        image_dir   = fromdir + train_image_dir + '3',
+        ann_dir     = fromdir + train_ann_dir + '3/',
+        samples     = samples3,
+        scale       = scale,
+        active_train= active_train,
+        changes     = changes
+    )
+
+
+    # Loading test dataset
+    val_images, val_labels, val_boxes = dataset(
+        image_dir   = fromdir + val_image_dir,
+        ann_dir     = fromdir + "/assignment_1/test/annotations/",
+        samples     = 1941,
+        scale       = scale,
+        active_train= active_train,
+        changes     = changes
+    ) 
+
+    
+
+    # Train model
+    if active_train:
+        datas = Dataset(train_images, train_labels, train_boxes)
+        datas2 = Dataset(train_images2, train_labels2, train_boxes2)
+        datas3 = Dataset(train_images3, train_labels3, train_boxes3)
+        valdataset = ValDataset(val_images, val_labels, val_boxes)
+
+        train(
+            num_of_epochs   = num_of_epochs,
+            lr              = lr,
+            dataset         = datas,
+            dataset2        = datas2,
+            dataset3        = datas3,
+            valdataset      = valdataset,
+            samples         = samples,
+            savedir         = savedir
+        )
+
+    # Predict
+    while(True):
+        imagename = input("Image:> ")
+        if imagename == "end":
+            exit(0)
+
+        predict(
+            image       = fromdir + val_image_dir + "/" + imagename + ".jpg", 
+            number_model= number_model,
+            scale       = scale,
+            showfinalimage = False,
+            saveimage      = True,
+            classes        = classes,
+            savedir        = savedir
+        )
+
+   #preso il return del predict, su 3000 foto vediamo quante hanno predetto giusto e aggiustiamo
+   #la soglia di confidenza, poi in seguito calcoliamo recall e tutto il resto con il quale
+   #creiamo la tabellina. Per LA intersection Union invece vediamo.  
---
+++ 3predict/predictor.py
@@ -0,0 +1,114 @@
+from network import Network
+import torch 
+import torch.nn as nn
+import torch.nn.functional as F
+import torch
+import os
+import torch.optim as optim
+import json
+import cv2
+import numpy as np
+import matplotlib.pyplot as plt
+
+def items(jsstring):
+    ret = []
+    for i in range(1,20):
+        if "item"+str(i) in jsstring:
+            ret.append(jsstring["item"+str(i)])
+    return ret
+
+def preprocess(img, image_size):
+    image = cv2.resize(img, (image_size, image_size))
+    image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
+    image = image.astype("float") / 255.0 
+
+    # Expand dimensions as predict expect image in batches
+    image = np.expand_dims(image, axis=0) 
+    return image
+
+def postprocess(image, results, classes, scale):
+    [class_probs, bounding_box, class_probs1, bounding_box1, class_probs2, bounding_box2] = results
+    class_index = torch.argmax(class_probs)
+    class_label = classes[class_index]
+
+    class_index1 = torch.argmax(class_probs1) + 5
+    class_label1 = classes[class_index1]
+
+    class_index2 = torch.argmax(class_probs2) + 9
+    class_label2 = classes[class_index2]
+    # Extract the Coordinates
+    x1, y1, x2, y2 = bounding_box[0]
+    x3, y3, x4, y4 = bounding_box1[0]
+    x5, y5, x6, y6 = bounding_box2[0]
+
+    # # Convert the coordinates from relative (i.e. 0-1) to actual values
+    x1 = int(scale * x1)
+    x2 = int(scale * x2)
+    y1 = int(scale * y1)
+    y2 = int(scale * y2)
+
+    x3 = int(scale * x3)
+    x4 = int(scale * x4)
+    y3 = int(scale * y3)
+    y4 = int(scale * y4)
+
+    x5 = int(scale * x5)
+    x6 = int(scale * x6)
+    y5 = int(scale * y5)
+    y6 = int(scale * y6)
+    # return the lable and coordinates
+
+    return class_label, (x1,y1,x2,y2), torch.max(class_probs)*100, class_label1, (x3,y3,x4,y4), torch.max(class_probs1)*100, class_label2, (x5,y5,x6,y6), torch.max(class_probs2)*100, 
+
+def predict(image, number_model, scale, showfinalimage, saveimage, classes, savedir):
+    device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
+    model = Network(True)
+    model = model.to(device)
+    model.load_state_dict(torch.load(savedir+"/model_ep"+str(number_model)+".pth"))
+    model.eval()
+    
+    # Reading Image
+    img  = cv2.imread(image)
+
+    # # Before we can make a prediction we need to preprocess the image.
+    processed_image = preprocess(img, scale)
+    result = model(torch.permute(torch.from_numpy(processed_image).float(),(0,3,1,2)).to(device))
+    scale = 448
+    # After postprocessing, we can easily use our results
+    label, (x1, y1, x2, y2), confidence, label1, (x3, y3, x4, y4), confidence1, label2, (x5, y5, x6, y6), confidence2 = postprocess(image, result, classes, scale)
+    
+    img = cv2.resize(img, (scale, scale))
+    img = cv2.rectangle(img, (x1, y1), (x2, y2), (0, 255, 100), 2)
+    img = cv2.rectangle(img, (x3, y3), (x4, y4), (255, 153, 51), 2)
+    img = cv2.rectangle(img, (x5, y5), (x6, y6), (0, 153, 51), 2)
+    cv2.putText(img, label, (x1, y1-10), cv2.FONT_HERSHEY_SIMPLEX, 0.7, (0, 255, 100), 2)
+    cv2.putText(img, label1, (x4-50, y4+20), cv2.FONT_HERSHEY_SIMPLEX, 0.7, (255, 153, 51), 2)
+    cv2.putText(img, label2, (x6-50, y6+20), cv2.FONT_HERSHEY_SIMPLEX, 0.7, (0, 153, 51), 2)
+    
+
+    #print("Informazioni")
+    print(image[-10:])
+    print(label, confidence.item())
+    print(label1, confidence1.item())
+    print(label2, confidence2.item())
+    imageinfo_filename = str(image[-10:-4]) + ".json"
+    
+    with open("./assignment_1/test/all_annotations/" + imageinfo_filename, 'r') as f:
+        data = json.loads(f.read())
+        array = items(data)
+    cat = ""
+    for item in array:
+        cat += str(item["category_name"]) + " - "
+    
+    cv2.putText(img, cat[0:-3], (10, 20), cv2.FONT_HERSHEY_SIMPLEX, 0.8, (0, 0, 0), 2)   
+
+    print("La foto contiene: " + str(cat))
+
+    # Showing and saving predicted
+    plt.imshow(img[:,:,::-1])
+    if saveimage:
+        plt.savefig("./immagini/" + image[-10:])
+    if showfinalimage:
+        plt.show()
+
+    #qui dobbiamo fare un bel return con quante ne predice giuste
